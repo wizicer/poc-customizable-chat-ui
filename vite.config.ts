e4775b7d5deea1018e5124350516d8314498d686
Liang, Shuang (Icer) <liangshuang@sutu.tech>
2026-03-20 10:05:34 +0900

    try to fix github 404

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-export default defineConfig({
-  base: "/poc-customizable-chat-ui/",
+export default defineConfig(({ command }) => ({
+  base: command === "build" ? "/poc-customizable-chat-ui/" : "/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
